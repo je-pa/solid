a88c73934ebd1d95f14125221a3f0fc9523205ca
je-pa <jieun0502@gmail.com>
2024-06-20 18:20:36 +0900

    ♻️ Refactor code: lsp 적용한 코드

--- src/main/java/lsp/after/PopSinger.java
+++ src/main/java/lsp/after/PopSinger.java
@@ -3,6 +3,10 @@ package lsp.after;
 public class PopSinger extends Singer{
   private String songType = "pop ";
 
+  public PopSinger(Composable composable) {
+    super(composable);
+  }
+
   @Override
   public void sing() {
     System.out.println(songType);
--- src/main/java/lsp/after/Singer.java
+++ src/main/java/lsp/after/Singer.java
@@ -1,7 +1,18 @@
 package lsp.after;
 
-public class Singer implements Composable{
+public class Singer {
+  private Composable composable;
+
+  public Singer(Composable composable) {
+    this.composable = composable;
+  }
+
   public void sing(){
     System.out.println("노래를 부른다.");
+  }
+
+  public void compose(){
+    if(composable == null) return;
+    composable.compose();
   }
 }
